ddfc8ad999b04ea546e92a66e4527a59b4f454fc
TheAngryPerson <86736586+TheAngryPerson@users.noreply.github.com>
2021-07-01 14:09:35 -0700

    Update BetterCallNotifications.plugin.js

--- Plugins/BetterCallNotifications/BetterCallNotifications.plugin.js
+++ Plugins/BetterCallNotifications/BetterCallNotifications.plugin.js
@@ -5,7 +5,7 @@
  * @version 0.0.1
  * @authorId 302960843711315968
  * @source https://github.com/TheAngryPerson/BetterDiscordStuff/tree/main/Plugins/BetterCallNotifications
- * @updateUrl https://github.com/TheAngryPerson/BetterDiscordStuff/blob/d9ac5f512b0c8813eadfe2175e6f66888d79800b/Plugins/BetterCallNotifications.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/TheAngryPerson/BetterDiscordStuff/d9ac5f512b0c8813eadfe2175e6f66888d79800b/Plugins/BetterCallNotifications.plugin.js
  */
 
 module.exports = class BetterCallNotifications {
