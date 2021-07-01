b3c7c58d10586d684d61ab8398fc93f4ffe9a1ad
TheAngryPerson <86736586+TheAngryPerson@users.noreply.github.com>
2021-07-01 14:07:00 -0700

    Update BetterCallNotifications.plugin.js

--- Plugins/BetterCallNotifications/BetterCallNotifications.plugin.js
+++ Plugins/BetterCallNotifications/BetterCallNotifications.plugin.js
@@ -1,14 +1,14 @@
 /**
  * @name BetterCallNotifications
  * @author Angry Person
- * @description Allows the user to disable call notifications even without dnd and enables call notifications to bypass dnd.
+ * @description DOES NOT WORK CURRENTLY Allows the user to disable call notifications even without dnd and enables call notifications to bypass dnd.
  * @version 0.0.1
  * @authorId 302960843711315968
  * @source https://github.com/TheAngryPerson/BetterDiscordStuff/tree/main/Plugins/BetterCallNotifications
  * @updateUrl https://github.com/TheAngryPerson/BetterDiscordStuff/blob/d9ac5f512b0c8813eadfe2175e6f66888d79800b/Plugins/BetterCallNotifications.plugin.js
  */
 
-module.exports = class MyPlugin {
-    start() {} // Required function. Called when the plugin is activated (including after reloads)
-    stop() {} // Required function. Called when the plugin is deactivated
+module.exports = class BetterCallNotifications {
+    start() {}
+    stop() {}
 }
